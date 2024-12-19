1fb26a8cb4a91324ad19b336bfc72fbe6d92cf02
Saydina Ambiya <saydina.ambiya@bankmandiri.co.id>
2024-12-19 14:46:03 +0700

    Refactor streaming a whole page

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -12,8 +12,6 @@ import {fetchRevenue} from "@/app/lib/data";
 export default async function RevenueChart() {
     const revenue = await fetchRevenue();
     const chartHeight = 350;
-    // NOTE: Uncomment this code in Chapter 7
-
     const {yAxisLabels, topLabel} = generateYAxis(revenue);
 
     if (!revenue || revenue.length === 0) {
